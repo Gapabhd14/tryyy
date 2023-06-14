a4f191827c8d30920c304fb9689ffc7d325d1dca
Gapabhd14 <gapa.bhd14@gmail.com>
2023-06-14 09:54:12 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div id="app">
+        <HelloWorld msg="You did it!" />
+        <h1>hh</h1>
+  </div>
+</template>
+
+<style scoped>
+  
+</style>
